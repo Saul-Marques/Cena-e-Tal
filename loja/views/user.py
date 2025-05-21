1d5c7a52697d62b2c3ef47e1a4331b41b49d3aa6
Saúl Marques <saul_marques10@hotmail.com>
2025-05-21 17:34:56 +0100

    Corrigido um bug

--- loja/views/user.py
+++ loja/views/user.py
@@ -3,24 +3,34 @@ from django.views import View
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from loja.models import User, Product
+from loja.models import User, Product, CIDADES_CHOICES
 
 @method_decorator(login_required, name='dispatch')
 class UserView(View):
     def get(self, request, user_id):
         perfil = get_object_or_404(User, id=user_id)
         produtos = Product.objects.filter(user=perfil)
-        return render(request, "users.html", {"perfil": perfil, "is_owner": perfil.id == request.user.id, "produtos": produtos})
+        return render(request, "users.html", {
+            "perfil": perfil,
+            "is_owner": perfil.id == request.user.id,
+            "produtos": produtos,
+            "cidades": CIDADES_CHOICES
+        })
 
     def post(self, request, user_id):
         perfil = get_object_or_404(User, id=user_id)
         if perfil.id != request.user.id:
             messages.error(request, "Não pode editar o perfil de outro utilizador.")
-            return render(request, "users.html", {"perfil": perfil, "is_owner": False})
+            return render(request, "users.html", {
+                "perfil": perfil,
+                "is_owner": False,
+                "cidades": CIDADES_CHOICES
+            })
 
+        # Atualiza os campos
         perfil.primeiro_nome = request.POST.get("primeiro_nome", perfil.primeiro_nome)
         if request.FILES.get("profile_picture"):
-            perfil.profile_picture = request.FILES["profile_picture"]        
+            perfil.profile_picture = request.FILES["profile_picture"]
         perfil.ultimo_nome = request.POST.get("ultimo_nome", perfil.ultimo_nome)
         perfil.biografia = request.POST.get("biografia", perfil.biografia)
         perfil.localidade = request.POST.get("endereco", perfil.localidade)
@@ -32,27 +42,10 @@ class UserView(View):
         perfil.save()
         messages.success(request, "Perfil atualizado com sucesso!")
 
-        return render(request, "users.html", {"perfil": perfil, "is_owner": True})
-
-    
-    @method_decorator(login_required)
-    def post(self, request):
-        user = request.user
-        user.primeiro_nome = request.POST.get("primeiro_nome", user.primeiro_nome)
-        if request.FILES.get("profile_picture"):
-            user.profile_picture = request.FILES["profile_picture"]        
-        user.ultimo_nome = request.POST.get("ultimo_nome", user.primeiro_nome)
-        user.biografia = request.POST.get("biografia", user.biografia)
-        user.localidade = request.POST.get("endereco", user.localidade)
-        user.cidade = request.POST.get("cidade", user.cidade)
-        user.cp = request.POST.get("cp", user.cp)
-        user.telemovel = request.POST.get("telemovel", user.telemovel)
-        user.email = request.POST.get("email", user.email)
-
-        user.save()
-        messages.success(request, "Perfil atualizado com sucesso!")
-
+        produtos = Product.objects.filter(user=perfil)
         return render(request, "users.html", {
-            "user": user,
+            "perfil": perfil,
+            "is_owner": True,
+            "produtos": produtos,
             "cidades": CIDADES_CHOICES
-            })
+        })
